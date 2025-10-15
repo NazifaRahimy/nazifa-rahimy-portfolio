f8e1f071f61ca86ec6ec21eeba3fc6a7621bb7aa
nazifaRahimi <nazifarahimy6@gmail.com>
2025-10-15 14:11:51 +0430

    update font-size for several elements

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -40,16 +40,16 @@ const Home = () => {
     return ( 
         <div name='home' className="w-full h-auto md:h-screen  bg-[linear-gradient(135deg,#fff8ec_0%,#f5d6a0_60%,#f0c97a_100%)]">
             <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full  px-4 pb-5 md:flex-row ">
-                <div className="flex flex-col justify-center pt-20 md:pt-0  w-full md:w-3/5  h-full pr-5">
-                    <h2 className=" text-3xl font-bold  py-2  ">
+                <div className="flex flex-col justify-center pt-10 md:pt-0  w-full md:w-3/5  h-full pr-5">
+                    <h3 className=" text-2xl py-1 font-semibold sm:text-3xl sm:font-bold  sm:py-2  ">
                         <span   style={{ textShadow: "0 0 8px black" }}>Front-</span>
                         <span className="text-[rgb(109,67,0)] " style={{ textShadow: "0 0 10px rgb(109,67,0)" }} >       
                             {displayText}
                             <span className="animate-pulse " >|</span> 
                         </span>
-                    </h2>
-                    <h4 className="text-3xl font-bold mt-5" data-aos="fade-right" data-aos-delay="800" >Build Modern, Responsive <span className=" text-[rgb(109,67,0)]">Websites</span> </h4>
-                    <h4 className="text-3xl font-bold  " data-aos="fade-left" data-aos-duration="1000" data-aos-delay="900">That Elevate Your <span className="text-[#f0c97a]"> Online Presence</span></h4>
+                    </h3>
+                    <h4 className="text-2xl sm:text-3xl font-bold mt-5" data-aos="fade-right" data-aos-delay="800" >Build Modern, Responsive <span className=" text-[rgb(109,67,0)]">Websites</span> </h4>
+                    <h4 className="text-2xl sm:text-3xl font-bold  " data-aos="fade-left" data-aos-duration="1000" data-aos-delay="900">That Elevate Your <span className="text-[#f0c97a]"> Online Presence</span></h4>
                     <p className="text-gray-500 py-4 text-xl font-normal " data-aos="flip-up" data-aos-duration="1000" data-aos-delay="1000"> I craft clean, interactive, and responsive user interfaces using HTML, CSS, Tailwind,JavaScript React and Next Js. Whether you're launching a brand or refreshing your current website, I can help you stand out online.</p>
                     <SocialLink />
                     <div>
@@ -61,9 +61,9 @@ const Home = () => {
                         </Link>
                     </div>
                 </div>
-                <div className="md:w-2/5 flex items-center justify-center "  data-aos="zoom-in-left" data-aos-duration="2500">
+                <div className="md:w-2/5 flex items-center justify-center  mt-5 md:mt-0"  data-aos="zoom-in-left" data-aos-duration="2500">
                     <div style={{ boxShadow: "0 0 30px rgb(109,67,0)" }} className=" border-box md:w-[85%]  rounded-full ">
-                        <img src="/nazifa.jpg" alt="my profile"  className="h-[400px] md:h-[450px] rounded-full" />
+                        <img src="/nazifa.jpg" alt="my profile"  className="h-[350px] sm:h-[400px] md:h-[450px] rounded-full" />
                     </div>
                 </div>
             </div>
