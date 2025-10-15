6048f446c64d2eee5926c585c519241941583bb8
nazifaRahimi <nazifarahimy6@gmail.com>
2025-10-15 14:13:35 +0430

    update background-color in components

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contact from './component/Contact';
 
 function App() {
    return (
-    <div className="absolute w-[100%] top-[0%] left-[0%] 2x:w-[100%] 2xl:bg-red-300  border-box">
+    <div className="absolute w-[100%] top-[0%] left-[0%] 2x:w-[100%] 2xl:bg-gray-200  border-box">
         < div className="relative mx-auto w-[100%] h-auto sm:w-[100%] sm:h-auto md:w-[100%] md:h-[100%] lg:w-[100%] lg:h-auto xl:w-[100%] xl:h-auto 2xl:w-[1366px] 2xl:h-auto ">
             < Navbar />
             <Home />
