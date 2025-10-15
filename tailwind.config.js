87f7aa7d302c8e0a7dfc4cad63dbdc7b719199ba
nazifaRahimi <nazifarahimy6@gmail.com>
2025-10-15 09:39:23 +0430

    add breakpoint for responsive my protfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+       screens: {
+      'sm': '600px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
+      '2xl': '1366px',
+    },
     extend: {},
         fontFamily: {
           signature:['Great Vibes'],
